fe97490b15e243131478b141a4362adb2000ec17
Mahmoud Hassan Hamed Okily <mahmoudhassanhamed.okily@studenti.unimi.it>
2021-01-16 18:09:06 +0100

    add novita css

--- client/pages/novita/index.js
+++ client/pages/novita/index.js
@@ -1,8 +1,13 @@
 /** @format */
 import { useEffect, useState } from "react";
 import generateProducts from "../../seeds/products";
-
+import Layout from '../../components/layout/layout'
+import styles from './Novita.module.css'
 function Novita() {
-  return <div>Novita page</div>;
+  return <Layout>
+    <div className={styles.container }>
+      novita
+    </div>
+  </Layout>
 }
 export default Novita;
